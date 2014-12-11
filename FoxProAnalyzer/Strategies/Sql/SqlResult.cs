5594c6fe6db66127c1695e16fb6a9ae5f4590de3
achort <andres.chort@gmail.com>
2014-12-11 15:11:44 -0300

    SqlStrategy

---
+++ FoxProAnalyzer/Strategies/Sql/SqlResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FoxProAnalyzer.Strategies.Sql
+{
+    public class SqlResult
+    {
+        public string Name { get; set; }
+
+        public int StoredProceduresCount { get; set; }
+
+        public int PlainQueryCount { get; set; }
+
+        public bool IsError { get; set; }
+
+        public string ErrorMessage { get; set; }
+    }
+}
